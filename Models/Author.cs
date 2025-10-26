929fe4c876631ba51c49e565e42839ca3b751f5b
Luka Iadze <l.ladze@andersenlab.com>
2025-10-26 20:43:52 +0400

    initial creation commit

---
+++ Models/Author.cs
@@ -0,0 +1,7 @@
+namespace upgaming_luka_iadze.Models;
+
+public class Author
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,9 @@
+namespace upgaming_luka_iadze.Models;
+
+public class Book
+{
+    public int ID { get; set; }
+    public string Title { get; set; }
+    public int AuthorID { get; set; } // Foreign Key
+    public int PublicationYear { get; set; }
+}
